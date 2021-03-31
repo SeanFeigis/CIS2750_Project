7a82fb3aec3a5f864c0372377526892c7cdeb8d0
Sean Shaya Feigis <sfeigis@uoguelph.ca>
2021-03-31 17:01:26 -0400

    help me

---
+++ public/index.js
@@ -0,0 +1,105 @@
+// Put all onload AJAX calls here, and event listeners
+jQuery(document).ready(function() {
+    // On page-load AJAX Example
+    jQuery.ajax({
+        type: 'get',            //Request type
+        dataType: 'json',       //Data type - we will use JSON for almost everything 
+        url: '/endpoint1',   //The server endpoint we are connecting to
+        data: {
+            data1: "Value 1",
+            data2:1234.56
+        },
+        success: function (data) {
+            /*  Do something with returned object
+                Note that what we get is an object, not a string, 
+                so we do not need to parse it on the server.
+                JavaScript really does handle JSONs seamlessly
+            */
+            jQuery('#blah').html("On page load, received string '"+data.somethingElse+"' from server");
+            //We write the object to the console to show that the request was successful
+            console.log(data); 
+
+        },
+        fail: function(error) {
+            // Non-200 return, do something with error
+            $('#blah').html("On page load, received error from server");
+            console.log(error); 
+        }
+    });
+
+    // Event listener form example , we can use this instead explicitly listening for events
+    // No redirects if possible
+    $('#DropDown').submit(function(e){
+        var x = document.getElementById("FileSelect");
+        var option = document.createElement("option");
+        option.text = "New File";
+        x.add(option, x[0]);
+        e.preventDefault();
+        //Pass data to the Ajax call, so it gets passed to the server
+        $.ajax({
+            //Create an object for connecting to another waypoint
+        });
+    });
+
+    $('#FileUploadForm').submit(function(e){
+        console.log('Uploaded File');
+        $('#blah').html("Cringe");
+        var table = document.getElementById("FileTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML = "NEW CELL1";
+        cell2.innerHTML = "NEW CELL2";
+        cell3.innerHTML = "NEW CELL3";
+        cell4.innerHTML = "NEW CELL4";
+        cell5.innerHTML = "NEW CELL5";
+        cell6.innerHTML = "NEW CELL6";
+
+        var x = document.getElementById("FileSelect");
+        var option = document.createElement("option");
+        option.text = "New File";
+        x.add(option, x[0]);
+
+        e.preventDefault();
+        //Pass data to the Ajax call, so it gets passed to the server
+        $.ajax({
+            //Create an object for connecting to another waypoint
+        });
+    });
+
+});
+
+function blah() {
+
+    jQuery.ajax({
+        type: 'get',            //Request type
+        dataType: 'json',       //Data type - we will use JSON for almost everything 
+        url: '/endpoint1',   //The server endpoint we are connecting to
+        data: {
+            data1: "Value 1",
+            data2:1234.56
+        },
+        success: function (data) {
+            /*  Do something with returned object
+                Note that what we get is an object, not a string, 
+                so we do not need to parse it on the server.
+                JavaScript really does handle JSONs seamlessly
+            */
+            jQuery('#blah').html("On page load, received string '"+data.somethingElse+"' from server");
+            //We write the object to the console to show that the request was successful
+            console.log(data); 
+
+        },
+        fail: function(error) {
+            // Non-200 return, do something with error
+            $('#blah').html("On page load, received error from server");
+            console.log(error); 
+        }
+    });
+
+
+}
